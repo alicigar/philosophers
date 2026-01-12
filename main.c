6c5a1cf9a0dc9bece9f8d9dd90aee14acab95265
Alicia Momoko Garcia Gokan <alicigar@c2r7s4.42malaga.com>
2026-01-12 19:37:07 +0100

    12/01

---
+++ main.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alicigar < alicigar@student.42malaga.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/12 16:25:47 by alicigar          #+#    #+#             */
+/*   Updated: 2026/01/12 18:29:24 by alicigar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	main(int argc, char **argv)
+{
+	//struct
+
+	//validar argumentos
+	if (argc != 5 && argc != 6)
+	{
+		
+	}
+	//guardar args en la estructura general (de str a int)
+	//inicializar todo, los mutexes, array de filosofos, tiempos iniciales
+	//crear los hilos de los filosofos
+	//crear el gilo monitor
+	//esperar a que termina la simulacion porque alguien murio o porque todos comieron x veces
+	//destruir mutex y liberar
+	return (0);
+}
+
+//init.c utils.c monitor.c ...
---
+++ philo.h
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alicigar < alicigar@student.42malaga.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/12 16:23:45 by alicigar          #+#    #+#             */
+/*   Updated: 2026/01/12 19:36:12 by alicigar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+//libs
+# include <string.h>//-> memset
+# include <stdio.h>//-> printf
+# include <stdlib.h>//-> malloc, free 
+# include <unistd.h>//-> write, usleep 
+# include <sys/time.h>//-> gettimeofday 
+# include <pthread.h>/*-> pthread_create, pthread_detach, 
+pthread_join, pthread_mutex_init, pthread_mutex_destroy,
+pthread_mutex_lock, pthread_mutex_unlock*/
+
+//structs
+DECLARAR LA ESTRUCTURA
+
+//ESTRUCTURA DEL FILOSOFO
+//id ✅
+//contador de comidas ✅
+//tiempo de ultima comida ✅
+//puntero a la ESTRUCTURA GENERAL ✅
+//su hilo ✅
+//indices de tenedor izqdo y tenedor dcho ✅
+
+typedef struct s_philo
+{
+	int				id;
+	int				meals;
+	long			last_meal; //long por overflow con int
+	pthread_t		thread;
+	struct s_data	*data;
+	int				left_fork;
+	int				right_fork;
+}	t_philo;
+
+//ESTRUCTURA GENERAL
+//parametros de entrada ✅
+//array de mutexes/tenedores 
+//mutex para imprimir 
+//array de filosofos 
+//tiempo de inicio 
+//flag de muerte 
+typedef struct s_data
+{
+	int				number_of_ohilosophers;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				must_eat;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	log_mutex;
+	t_philo			*philos;
+	long			start_time;
+	int				dead;
+}	t_data;
+
+//functions
+
+#endif
