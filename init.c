defe209955e0df1803a1093106a8833d0f383c42
Alicia Momoko Garcia Gokan <alicigar@c2r8s2.42malaga.com>
2026-01-14 22:40:06 +0100

    14/01

--- init.c
+++ init.c
@@ -6,7 +6,40 @@
 /*   By: alicigar < alicigar@student.42malaga.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 21:42:37 by alicigar          #+#    #+#             */
-/*   Updated: 2026/01/13 21:42:58 by alicigar         ###   ########.fr       */
+/*   Updated: 2026/01/14 22:39:06 by alicigar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "philo.h"
+
+int	init_data(t_data *data, int argc, char **argv)
+{
+	data->number_of_philosophers = ft_atoi(argv[1]);
+	data->time_to_die = ft_atoi(argv[2]);
+	data->time_to_eat = ft_atoi(argv[3]);
+	data->time_to_sleep = ft_atoi(argv[4]);
+
+	if (argc == 6)
+		data->must_eat = ft_atoi(argv[5]);
+	else
+		data->must_eat = -1;
+	data->dead = 0;
+	data->start_time = //get_time()//;
+	return (0);
+}
+
+/*
+typedef struct s_data
+{
+	int				number_of_philosophers;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				must_eat;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	log_mutex;
+	t_philo			*philos;
+	long			start_time;
+	int				dead;
+}	t_data;
+*/
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: alicigar < alicigar@student.42malaga.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 16:25:47 by alicigar          #+#    #+#             */
-/*   Updated: 2026/01/14 20:25:18 by alicigar         ###   ########.fr       */
+/*   Updated: 2026/01/14 22:39:28 by alicigar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	main(int argc, char **argv)
 {
-	t_data	data;
+	//t_data	data;
 
 	if (argc < 5 || argc > 6)
 		return (1);
